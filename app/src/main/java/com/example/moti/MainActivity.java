a5b0bcf63c96d9e71be8744d602ecfb7de345e08
MuthonduG <muthondugithinji@gmail.com>
2025-11-15 03:12:51 +0300

    updates: finished setting up onboarding

--- app/src/main/java/com/example/moti/MainActivity.java
+++ app/src/main/java/com/example/moti/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.moti;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView initialSignin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // get components by IDs
+        initialSignin = findViewById(R.id.initial_sigin);
+
+        initialSignin.setOnClickListener( v -> {
+            Intent intent = new Intent(MainActivity.this, login.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
